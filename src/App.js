2348cc088f929d52e4876517fee8d1e15db90574
unknown <alokkum7010@gmail.com>
2025-06-10 14:33:06 +0530

    xstable

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import XTable from "./components/xtable";
+function App() {
+  return (
+    <div>
+      <h1>Date and Views Table</h1>
+      <XTable />
+    </div>
+  );
+}
+
+export default App;
